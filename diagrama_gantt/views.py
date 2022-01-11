26c2941999307ce67acc65872f2e96ede3ff4cf9
Miguel Muñoz <jose.munoz01@epn.edu.ec>
2022-01-10 19:59:32 -0500

    Update: diagrama_gantt.html

--- diagrama_gantt/views.py
+++ diagrama_gantt/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from diagrama_gantt.models import Proyecto, Tarea, Estado
+
 
 # Create your views here.
+def index(request):
+    #TODO Definir a que proyecto se hace referencia
+    proyectoID = 1
+
+    proyecto = Proyecto.objects.get(id=proyectoID)
+    tareas = Tarea.objects.filter(proyecto=proyectoID)
+    #'tareas': tareas
+    return render(request, 'diagrama_gantt.html',{'proyecto': proyecto, 'tareas': tareas})
